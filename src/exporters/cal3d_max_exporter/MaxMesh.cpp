3fee339a6315830fa8529a234f8abbcd853131f9
gaylord.foureau <gaylord.foureau@977ac63d-422d-a444-7582-39043becfd45>
2010-07-14 08:50:41 +0000

    Invert V coordinate of textures

--- src/exporters/cal3d_max_exporter/MaxMesh.cpp
+++ src/exporters/cal3d_max_exporter/MaxMesh.cpp
@@ -528,7 +528,7 @@ CVertexCandidate *CMaxMesh::GetVertexCandidate(CSkeletonCandidate *pSkeletonCand
 				}
 
 				// add texture coordinate to the vertex candidate
-				pVertexCandidate->AddTextureCoordinate(uvVert.x, uvVert.y);
+				pVertexCandidate->AddTextureCoordinate(uvVert.x, 1.0f - uvVert.y);
 			}
 		}
 	}
